9cd3f914dfa6e2f893de1cf21b4c23f3ad465444
Donghyeon Jeong <50125695+dhje0ng@users.noreply.github.com>
2025-06-29 20:19:30 +0900

    Update: site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Blog (Korean)`,
+      href: "https://until.blog/@dhjeong",
     },
   ],
   // blog setting (required)
